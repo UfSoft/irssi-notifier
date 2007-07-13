029e3382d1dc4adbe2ae689998f7a377d5415f97
Pedro Algarvio <ufs@ufsoft.org>
2007-07-13 16:44:11 +0100

    [svn r10] Version Bump. ChangeLog updated.

--- irssinotifier/__init__.py
+++ irssinotifier/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # vim: sw=4 ts=4 fenc=utf-8
 # =============================================================================
-# $Id: __init__.py 8 2007-07-13 14:23:18Z s0undt3ch $
+# $Id: __init__.py 10 2007-07-13 15:44:11Z s0undt3ch $
 # =============================================================================
 #             $URL: http://irssinotifier.ufsoft.org/svn/trunk/irssinotifier/__init__.py $
-# $LastChangedDate: 2007-07-13 15:23:18 +0100 (Fri, 13 Jul 2007) $
-#             $Rev: 8 $
+# $LastChangedDate: 2007-07-13 16:44:11 +0100 (Fri, 13 Jul 2007) $
+#             $Rev: 10 $
 #   $LastChangedBy: s0undt3ch $
 # =============================================================================
 # Copyright (C) 2007 UfSoft.org - Pedro Algarvio <ufs@ufsoft.org>
@@ -13,7 +13,7 @@
 # Please view LICENSE for additional licensing information.
 # =============================================================================
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__  = 'Pedro Algarvio'
 __email__   = 'ufs@ufsoft.org'
 __package__ = 'IrssiNotifier'
